45b5cacbdaa719b2d52ad761a501564c02b0878d
YongjuLee6565 <ivan.yongju.lee@gmail.com>
2022-01-03 10:16:31 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className="App">h1</div>;
 }
 
 export default App;
